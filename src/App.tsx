41f593ee5840375ccb37b195e5e87400987618ba
Abhishek Mishra <95057637+abhishek-mishraji@users.noreply.github.com>
2024-07-26 20:38:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/Button";
 export default function App() {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Cosden Solutions</h1>
+     
 
     </div>
   );
